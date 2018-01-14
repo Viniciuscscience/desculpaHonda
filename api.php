56fea23c469ff6541b691c6621231e655d8c79a0
vinicius <vinicius.cscience@gmail.com>
2018-01-14 18:40:54 -0300

    Ok

--- api.php
+++ api.php
@@ -38,9 +38,7 @@ switch ($method) {
     if($action == "login"){
        $name = $input['name'];
        $password = $input['password'];
-       $sql = "select * from `$table` WHERE name=$name AND password=$password";
-       print_r($sql);
-       exit;
+       $sql = "select * from `$table` WHERE name=`$name` AND password=`$password`";
        break;
 
     }else{
